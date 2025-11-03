386346db7123703fbe15f26f88ec0f2cd42aef30
S <2438990@cognizant.com>
2025-11-03 18:40:13 +0530

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const bookRoutes = require('./route');
+const bookRoutes = require('./route'); // Make sure this file is named route.js
 
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/library', {
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/books', bookRoutes);
 
