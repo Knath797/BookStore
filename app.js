94f855dc22e0da8dba37a04fdc55a9a72dd2ea11
S <2438990@cognizant.com>
2025-11-03 18:24:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bookRoutes = require('./route');
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/library', {
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/books', bookRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
